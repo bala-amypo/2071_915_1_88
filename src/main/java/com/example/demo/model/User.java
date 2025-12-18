a248b271d1223b03c0fd9ba04c2e0e0e8ac402a2
bala-amypo <bala@amypo.in>
2025-12-18 16:03:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String role = "RESIDENT";
+
+    @OneToOne(mappedBy = "owner")
+    private ApartmentUnit apartUnit;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, String role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() { 
+        return id; 
+    }
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+
+    public String getEmail() { 
+        return email; 
+    }
+
+    public String getRole() {
+         return role; 
+        }
+    public void setRole(String role) { 
+        this.role = role; 
+    }
+
+    public String getPassword() { 
+        return password;
+     }
+
+    public void setPassword(String password) {
+         this.password = password;
+     }
+
+    public void setApartmentUnit(ApartmentUnit apartUnit) {
+        this.apartUnit = apartUnit;
+    }
+} 
