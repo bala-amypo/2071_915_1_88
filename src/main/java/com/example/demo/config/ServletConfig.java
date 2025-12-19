72c9a1bb14c138aa0bb9aab967997c5e6f3d8307
bala-amypo <bala@amypo.in>
2025-12-19 09:48:33 +0000

    User commit

--- src/main/java/com/example/demo/config/ServletConfig.java
+++ src/main/java/com/example/demo/config/ServletConfig.java
@@ -1,5 +1,14 @@
 package com.example.demo.config;
 
-public class ServletConfig {
-    
+import org.springframework.context.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
