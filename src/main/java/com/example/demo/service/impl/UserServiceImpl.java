7c88f8e1c8982b2e1e36f3be936c4905e37b1040
bala-amypo <bala@amypo.in>
2025-12-26 08:12:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.exception.BadRequestException;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -41,7 +42,12 @@ public class UserServiceImpl implements UserService {
     public User updateRole(Long userId, String role) {
 
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "User not found"
+                        )
+                );
 
         user.setRole(role);
         return userRepository.save(user);
@@ -51,7 +57,12 @@ public class UserServiceImpl implements UserService {
     public User getUserById(Long id) {
 
         return userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "User not found"
+                        )
+                );
     }
 
     @Override
@@ -63,7 +74,6 @@ public class UserServiceImpl implements UserService {
             return false;
         }
 
-        User user = userOpt.get();
-        return passwordEncoder.matches(password, user.getPassword());
+        return passwordEncoder.matches(password, userOpt.get().getPassword());
     }
 }
